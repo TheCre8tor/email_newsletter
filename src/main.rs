6f02967c7451f2fe8df6c62f95874719c6b91fa3
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-12-18 12:41:22 +0100

    Created the main server for actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use std::io;
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    // HttpServer handles all transport level concerns -->
+    HttpServer::new(|| {
+        /* App is where all application logic lives:
+           routing, middlewares, request handlers, etc */ 
+        App::new()
+        .route("/", web::get().to(greet))
+        .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
