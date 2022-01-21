e44b2ded39004cf4fd6daa891e13df7549f81017
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-01-21 05:31:12 +0100

    Completely deployed to DigitalOcean Platform

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,11 +1,13 @@
 //! src/configurations.rs
 
 use config::{Config, ConfigError, File};
-use std::env;
 
 use secrecy::ExposeSecret;
 use secrecy::Secret;
 use serde::Deserialize;
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
+use sqlx::ConnectOptions;
 
 #[derive(Deserialize)]
 pub struct Settings {
@@ -15,18 +17,45 @@ pub struct Settings {
 
 #[derive(Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
 
 #[derive(Deserialize)]
 pub struct DatabaseSettings {
-    pub username: String,
-    pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
+    pub username: String,
+    pub password: Secret<String>,
     pub database_name: String,
     pub require_ssl: bool,
+}
+
+impl DatabaseSettings {
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            // Try an encrypted connection, fallback to unencrypted if it fails
+            PgSslMode::Prefer
+        };
+
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
+    }
+
+    pub fn with_db(&self) -> PgConnectOptions {
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing::log::LevelFilter::Trace);
+
+        options
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, ConfigError> {
@@ -48,6 +77,10 @@ pub fn get_configuration() -> Result<Settings, ConfigError> {
     settings.merge(
         config::File::from(configuration_directory.join(environment.as_str())).required(true),
     )?;
+
+    // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+    // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
 
     settings.try_into()
 }
@@ -79,29 +112,5 @@ impl TryFrom<String> for Environment {
                 other
             )),
         }
-    }
-}
-
-impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        // format method interpolate string -->
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.database_name
-        ))
-    }
-
-    pub fn connection_string_without_db(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port
-        ))
     }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,5 @@
 use crate::routes::{health_check, subscribe};
 
-use actix_web::middleware::Logger;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::io::Error;
